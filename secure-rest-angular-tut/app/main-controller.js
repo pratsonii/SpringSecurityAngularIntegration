2edfade3b61c963bf58c7557d63c4f657c771287
pratik <pratiksonii@yahoo.in>
2016-06-25 16:01:38 +0530

    Angular created

---
+++ secure-rest-angular-tut/app/main-controller.js
@@ -0,0 +1,114 @@
+'use strict';
+
+angular.module('secure-rest-angular-tut').controller('MainCtrl', function ($cookies, $http, $location, $q, $resource, $scope, Cookies, Csrf, Login) {
+
+	$scope.greetings = {
+		open: {
+			getResult: '',
+			postValue: 'some value'
+		},
+		secure: {
+			getResult: '',
+			postValue: 'some secure value'
+		}
+	};
+
+	$scope.credentials = {
+		username: '',
+		password: ''
+	};
+
+	var openResources = $resource('http://localhost:8080/rest/open', {}, {
+		get: {method: 'GET', cache: false, isArray: false},
+		post: {method: 'POST', isArray: false}
+	});
+
+	$scope.getOpenGreetings = function() {
+		$scope.greetings.open.getResult = '';
+
+		openResources.get().$promise.then(function (response) {
+			console.log('GET /rest/open returned: ', response);
+			$scope.greetings.open.getResult = response.greetings;
+		});
+	};
+
+	$scope.postOpenGreetings = function() {
+		openResources.post({greetings: $scope.greetings.open.postValue}).$promise.then(function(response) {
+			console.log('POST /rest/open returned: ', response);
+			console.info('You might want to check the server logs to see that the POST has been handled!');
+		});
+	};
+
+	$scope.login = function () {
+		Login.login($scope.credentials.username, $scope.credentials.password, function (data, status, headers, config) {
+			// Success handler
+			console.info('The user has been successfully logged in! ', data, status, headers, config);
+
+		}, function(data, status, headers, config) {
+			// Failure handler
+			console.error('Something went wrong while trying to login... ', data, status, headers, config);
+		});
+	};
+
+	$scope.logout = function() {
+		Login.logout(function (data, status, headers, config) {
+			// Success handler
+			$scope.credentials = {username: '', password: ''};
+			delete $cookies['JSESSIONID'];
+			console.info('The user has been logged out!');
+
+			$location.url('/');
+
+		}, function(data, status, headers, config) {
+			// Failure handler
+			console.error('Something went wrong while trying to logout... ', data, status, headers, config);
+		});
+	};
+
+	var secureResources = function (headers) {
+		if (headers !== undefined) {
+			return $resource('http://localhost:8080/rest/secure', {}, {
+				post: {method: 'POST', headers: headers, isArray: false}
+			});
+		} else {
+			return $resource('http://localhost:8080/rest/secure', {}, {
+				get: {method: 'GET', cache: false, isArray: false},
+				options: {method: 'OPTIONS', cache: false}
+			});
+		}
+	};
+
+	$scope.getSecureGreetings = function() {
+		$scope.greetings.secure.getResult = '';
+
+		secureResources().get().$promise.then(function (response) {
+			console.log('GET /rest/secure returned: ', response);
+			$scope.greetings.secure.getResult = response.greetings;
+
+		}).catch(function(response) {
+			handleError(response);
+		});
+	};
+
+	$scope.postSecureGreetings = function () {
+		Csrf.addResourcesCsrfToHeaders(secureResources().options, $http.defaults.headers.post).then(function (headers) {
+			secureResources(headers).post({greetings: $scope.greetings.secure.postValue}).$promise.then(function (response) {
+				console.log('POST /rest/secure returned: ', response);
+				console.info('You might want to check the server logs to see that the POST has been handled!');
+
+			}).catch(function(response) {
+				handleError(response);
+			});
+		});
+	};
+
+	var handleError = function(response) {
+
+		if (response.status === 401) {
+			console.error('You need to login first!');
+
+		} else {
+			console.error('Something went wrong...', response);
+		}
+	};
+});
---
+++ secure-rest-angular-tut/app/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('secure-rest-angular-tut', [
+	'ngCookies',
+	'ngResource'
+]);
+
+angular.module('secure-rest-angular-tut').config(['$httpProvider', function ($httpProvider) {
+
+	$httpProvider.defaults.withCredentials = true;
+	// Tough luck: the default cookie-to-header mechanism is not working for cross-origin requests!
+	$httpProvider.defaults.xsrfCookieName = 'CSRF-TOKEN'; // The name of the cookie sent by the server
+	$httpProvider.defaults.xsrfHeaderName = 'X-CSRF-TOKEN'; // The default header name picked up by Spring Security
+
+}]);
